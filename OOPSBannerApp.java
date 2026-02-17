525f6429da8ed09b8fea1607069a0b12f285cad8
rohitkanakala <rohitkanakala@gmail.com>
2026-02-17 09:13:03 +0530

    UC5: Render OOPS using Inline Array Initialization with String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,22 @@
 public class OOPSBannerApp {
-    public static void main(String[] args){
-        String[] lines = new String[7];
-        lines[0] = String.join(" ", "   ***   ", "   ***   ", "******   ", "   ***** ");
-        lines[1] = String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      ");
-        lines[2] = String.join(" ", "**     **", "**     **", "**     **", "**       ");
-        lines[3] = String.join(" ", "**     **", "**     **", "**   **  ", "  *****  "); 
-        lines[4] = String.join(" ", "**     **", "**     **", "******   ", "      ** ");
-        lines[5] = String.join(" ", " **   ** ", " **   ** ", "**       ", " **   ** ");
-        lines[6] = String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ");
-        for(String line : lines){
+
+    public static void main(String[] args) {
+
+        // Inline array initialization with String.join()
+        String[] lines = {
+
+            String.join("  ", " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join("  ", "*     *", "*     *", "*     *", "*      "),
+            String.join("  ", "*     *", "*     *", "*     *", "*      "),
+            String.join("  ", "*     *", "*     *", " ***** ", " ***** "),
+            String.join("  ", "*     *", "*     * ","*      ", "      *"),
+            String.join("  ", "*     *", "*     * ","*      ", "      *"),
+            String.join("  ", " ***** ", " *****  ","*      ",  "******")
+
+        };
+
+        // Enhanced for loop to print banner
+        for (String line : lines) {
             System.out.println(line);
         }
     }
